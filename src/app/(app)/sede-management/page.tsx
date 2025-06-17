3c5fef253d08ae75e4bea03e34abf411e82026b2
Jonathan Pastora <jonathanpastora802@gmail.com>
2025-06-17 16:59:13 +0000

    por que tardas mucho?

--- src/app/(app)/sede-management/page.tsx
+++ src/app/(app)/sede-management/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import {
   Table,
   TableBody,
